f4f22ca80bdffee67194f9cd99ea3e5beb6dc9bd
omarmosid <omar.mo.sid@gmail.com>
2020-05-14 01:41:34 +0530

    adds cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require("express");
+const cors = require('cors')
 const app = express();
 const path = require('path');
 const connectDB = require("./config/db");
@@ -8,6 +9,7 @@ const connectDB = require("./config/db");
 connectDB();
 
 // Init Middleware
+app.use(cors())
 app.use(express.json());
 
 app.use("/api/tickets", require('./routes/api/ticket'))
